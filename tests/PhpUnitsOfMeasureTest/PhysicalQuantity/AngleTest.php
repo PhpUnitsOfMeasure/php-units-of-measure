80a5cbbecbfeacfe4d9be47bb25ca5d56f6b4a37
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-26 14:28:17 +0800

    anonying CS

--- tests/PhpUnitsOfMeasureTest/PhysicalQuantity/AngleTest.php
+++ tests/PhpUnitsOfMeasureTest/PhysicalQuantity/AngleTest.php
@@ -4,11 +4,9 @@ namespace PhpUnitsOfMeasure\PhysicalQuantity;
 
 class AngleTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testToDegrees()
     {
         $angle = new Angle(2 * M_PI, 'rads');
         $this->assertEquals(360, $angle->toUnit('deg'));
     }
-
 }
