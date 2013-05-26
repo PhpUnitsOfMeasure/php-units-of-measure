1d988b9eccaff793c8e35037a8392e14204f979a
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-26 14:32:49 +0800

    test for radians conversion

--- tests/PhpUnitsOfMeasureTest/PhysicalQuantity/AngleTest.php
+++ tests/PhpUnitsOfMeasureTest/PhysicalQuantity/AngleTest.php
@@ -9,4 +9,10 @@ class AngleTest extends \PHPUnit_Framework_TestCase
         $angle = new Angle(2 * M_PI, 'rads');
         $this->assertEquals(360, $angle->toUnit('deg'));
     }
+
+    public function testToRadians()
+    {
+        $angle = new Angle(720, 'degree');
+        $this->assertEquals(M_PI * 4, $angle->toUnit('rad'));
+    }
 }
