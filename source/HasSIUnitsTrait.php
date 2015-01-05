4b97cc468c3b03ea6699b3dc0397b6e77d168937
Jonathan Hanson <jonathan@jonathan-hanson.org>
2015-01-04 20:12:33 -0800

    precise terminology!

--- source/HasSIUnitsTrait.php
+++ source/HasSIUnitsTrait.php
@@ -158,7 +158,7 @@ trait HasSIUnitsTrait
 
         // For each of the standard SI prefixes, attempt to register a new unit of measure
         foreach ($siPrefixes as $prefixDefinition) {
-            // Build a closure for resolving a pattern into a unit name
+            // Build a function for resolving a pattern into a unit name
             $parsePattern = function ($pattern) use ($prefixDefinition) {
                 return strtr(
                     $pattern,
