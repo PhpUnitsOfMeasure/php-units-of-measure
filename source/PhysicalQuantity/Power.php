69a8ccb34243227e95046889dd9f3a047a79d40d
Martin Lindhe <martin@ubique.se>
2015-05-10 12:33:00 +0200

    add Power unit

---
+++ source/PhysicalQuantity/Power.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PhpUnitsOfMeasure\PhysicalQuantity;
+
+use PhpUnitsOfMeasure\AbstractPhysicalQuantity;
+use PhpUnitsOfMeasure\UnitOfMeasure;
+use PhpUnitsOfMeasure\HasSIUnitsTrait;
+
+class Power extends AbstractPhysicalQuantity
+{
+    use HasSIUnitsTrait;
+
+    protected static $unitDefinitions;
+
+    protected static function initialize()
+    {
+        // watt
+        $kiloWatt = UnitOfMeasure::nativeUnitFactory('W');
+        $kiloWatt->addAlias('watt');
+        $kiloWatt->addAlias('watts');
+        static::addUnit($kiloWatt);
+
+        static::addMissingSIPrefixedUnits(
+            $kiloWatt,
+            1,
+            '%pW',
+            [
+                '%Pwatt',
+                '%Pwatts',
+            ]
+        );
+
+        $metricHorsePower = UnitOfMeasure::linearUnitFactory('PS', 735.49875);
+        $metricHorsePower->addAlias('pferdstärker'); // aka "metric horsepower"
+
+        static::addUnit($metricHorsePower);
+    }
+}
