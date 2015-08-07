ffb1ecf4fefb97d2af63aa8d5374a5c85d8957e1
Martin Lindhe <martin@ubique.se>
2015-08-07 11:22:12 +0200

    use singular form of Pferdestärke

--- source/PhysicalQuantity/Power.php
+++ source/PhysicalQuantity/Power.php
@@ -31,7 +31,7 @@ class Power extends AbstractPhysicalQuantity
         );
 
         $metricHorsePower = UnitOfMeasure::linearUnitFactory('PS', 735.49875);
-        $metricHorsePower->addAlias('pferdstärker'); // aka "metric horsepower"
+        $metricHorsePower->addAlias('pferdstärke'); // aka "metric horsepower"
 
         static::addUnit($metricHorsePower);
     }
