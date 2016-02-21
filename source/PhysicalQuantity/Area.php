8f9566b0b9555e5f26d6a40bb3a972f45cf60a8d
Todd Nestor <todd.nestor@gmail.com>
2016-02-20 21:21:40 -0800

    got rid of tabs and used spaces instead

--- source/PhysicalQuantity/Area.php
+++ source/PhysicalQuantity/Area.php
@@ -14,8 +14,8 @@ class Area extends AbstractPhysicalQuantity
         $metersquared = UnitOfMeasure::nativeUnitFactory('m^2');
         $metersquared->addAlias('m²');
         $metersquared->addAlias('meter squared');
-		$metersquared->addAlias('square meter');
-		$metersquared->addAlias('square meters');
+        $metersquared->addAlias('square meter');
+        $metersquared->addAlias('square meters');
         $metersquared->addAlias('meters squared');
         $metersquared->addAlias('metre squared');
         $metersquared->addAlias('metres squared');
@@ -25,8 +25,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit = UnitOfMeasure::linearUnitFactory('mm^2', 1e-6);
         $newUnit->addAlias('mm²');
         $newUnit->addAlias('millimeter squared');
-		$newUnit->addAlias('square millimeter');
-		$newUnit->addAlias('square millimeters');
+        $newUnit->addAlias('square millimeter');
+        $newUnit->addAlias('square millimeters');
         $newUnit->addAlias('millimeters squared');
         $newUnit->addAlias('millimetre squared');
         $newUnit->addAlias('millimetres squared');
@@ -36,8 +36,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit = UnitOfMeasure::linearUnitFactory('cm^2', 1e-4);
         $newUnit->addAlias('cm²');
         $newUnit->addAlias('centimeter squared');
-		$newUnit->addAlias('square centimeter');
-		$newUnit->addAlias('square centimeters');
+        $newUnit->addAlias('square centimeter');
+        $newUnit->addAlias('square centimeters');
         $newUnit->addAlias('centimeters squared');
         $newUnit->addAlias('centimetre squared');
         $newUnit->addAlias('centimetres squared');
@@ -47,8 +47,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit = UnitOfMeasure::linearUnitFactory('dm^2', 1e-2);
         $newUnit->addAlias('dm²');
         $newUnit->addAlias('decimeter squared');
-		$newUnit->addAlias('square decimeters');
-		$newUnit->addAlias('square decimeter');
+        $newUnit->addAlias('square decimeters');
+        $newUnit->addAlias('square decimeter');
         $newUnit->addAlias('decimeters squared');
         $newUnit->addAlias('decimetre squared');
         $newUnit->addAlias('decimetres squared');
@@ -59,8 +59,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit->addAlias('km²');
         $newUnit->addAlias('kilometer squared');
         $newUnit->addAlias('kilometers squared');
-		$newUnit->addAlias('square kilometer');
-		$newUnit->addAlias('square kilometers');
+        $newUnit->addAlias('square kilometer');
+        $newUnit->addAlias('square kilometers');
         $newUnit->addAlias('kilometre squared');
         $newUnit->addAlias('kilometres squared');
         static::addUnit($newUnit);
@@ -69,8 +69,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit = UnitOfMeasure::linearUnitFactory('ft^2', 9.290304e-2);
         $newUnit->addAlias('ft²');
         $newUnit->addAlias('foot squared');
-		$newUnit->addAlias('square foot');
-		$newUnit->addAlias('square feet');
+        $newUnit->addAlias('square foot');
+        $newUnit->addAlias('square feet');
         $newUnit->addAlias('feet squared');
         static::addUnit($newUnit);
 
@@ -78,8 +78,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit = UnitOfMeasure::linearUnitFactory('in^2', 6.4516e-4);
         $newUnit->addAlias('in²');
         $newUnit->addAlias('inch squared');
-		$newUnit->addAlias('square inch');
-		$newUnit->addAlias('square inches');
+        $newUnit->addAlias('square inch');
+        $newUnit->addAlias('square inches');
         $newUnit->addAlias('inches squared');
         static::addUnit($newUnit);
 
@@ -88,8 +88,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit->addAlias('mi²');
         $newUnit->addAlias('mile squared');
         $newUnit->addAlias('miles squared');
-		$newUnit->addAlias('square mile');
-		$newUnit->addAlias('square miles');
+        $newUnit->addAlias('square mile');
+        $newUnit->addAlias('square miles');
         static::addUnit($newUnit);
 
         // Yard squared
@@ -97,8 +97,8 @@ class Area extends AbstractPhysicalQuantity
         $newUnit->addAlias('yd²');
         $newUnit->addAlias('yard squared');
         $newUnit->addAlias('yards squared');
-		$newUnit->addAlias('square yard');
-		$newUnit->addAlias('square yards');
+        $newUnit->addAlias('square yard');
+        $newUnit->addAlias('square yards');
         static::addUnit($newUnit);
 
         // Are
