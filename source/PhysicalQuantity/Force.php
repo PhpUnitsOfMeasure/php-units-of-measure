d2849d5b710faa117e1a21c51b2454eb8a0d91b2
Oli Mortimer <olimortimer@gmail.com>
2016-09-14 16:53:58 +0100

    Force conversion

--- source/PhysicalQuantity/Force.php
+++ source/PhysicalQuantity/Force.php
@@ -3,15 +3,34 @@ namespace PhpUnitsOfMeasure\PhysicalQuantity;
 
 use PhpUnitsOfMeasure\AbstractPhysicalQuantity;
 use PhpUnitsOfMeasure\UnitOfMeasure;
+use PhpUnitsOfMeasure\HasSIUnitsTrait;
 
 class Force extends AbstractPhysicalQuantity
 {
+	use HasSIUnitsTrait;
+
 	protected static $unitDefinitions;
 
 	protected static function initialize()
 	{
+
+		// newton
+		$newton = UnitOfMeasure::nativeUnitFactory('N');
+		$newton->addAlias('newton');
+		static::addUnit($newton);
+
+		static::addMissingSIPrefixedUnits(
+			$newton,
+			1,
+			'%pN',
+			[
+				'%Pnewton',
+				'%Pnewtons'
+			]
+		);
+
 		// kilonewton
-		$newUnit = UnitOfMeasure::nativeUnitFactory('kN');
+		$newUnit = UnitOfMeasure::linearUnitFactory('kN', 1000);
 		$newUnit->addAlias('kilonewton');
 		static::addUnit($newUnit);
 
